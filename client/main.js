1aa6102f13f7a04491e4781a36e05cd3c8f63d0d
Josep Servat <josep.servat@guidesmiths.com>
2017-05-16 01:15:46 +0200

    First React Render

--- client/main.js
+++ client/main.js
@@ -1,6 +1,10 @@
-import someDefault , { greetUser, name} from './../imports/utils';
+import React from 'react';
+import ReactDom from 'react-dom';
+import {Meteor} from 'meteor/meteor';
 
-console.log("Log from Client js");
-console.log(greetUser());
-console.log(name);
-console.log(someDefault);
+
+// the callback in startup methos is run when the dom is ready
+Meteor.startup(function (){
+  let jsx = <p>This is from main.js client</p>;
+  ReactDom.render(jsx, document.getElementById('app'));
+});
